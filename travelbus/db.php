59f0047c31f12103322478cecd3b1c5d29cdbade
Developer Merah <112598535+DeveloperMerah@users.noreply.github.com>
2022-09-01 09:16:29 +0700

    Update db.php
    Menggunakan PDO

--- travelbus/db.php
+++ travelbus/db.php
@@ -1,10 +1,14 @@
 <?php
-    $mysql_hostname = "localhost";
-    $mysql_user = "root";
-    $mysql_password = "";
-    $mysql_database = "travelbus2";
-    $prefix = "";
-    $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or 
-    die("Could not connect database");
-?>
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=travelbus2", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
